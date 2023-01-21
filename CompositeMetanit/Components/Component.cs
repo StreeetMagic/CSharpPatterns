aa60b630d69c9b0c839d7729adc5f1abdafcea1a
Vlad Parastaev <streeetmagic@gmail.com>
2023-01-21 12:22:57 +0400

    Added Composite

---
+++ CompositeMetanit/Components/Component.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CompositeMetanit
+{
+    internal abstract class Component
+    {
+        protected readonly string Name;
+
+        protected Component(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void Add(Component component)
+        {
+            
+        }
+
+        public virtual void Remove(Component component)
+        {
+            
+        }
+ 
+        public virtual void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
---
+++ CompositeMetanit/Components/Directory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompositeMetanit
+{
+    internal class Directory :Component
+    {
+        private readonly List<Component> _components = new List<Component>();
+ 
+        public Directory(string name)
+            : base(name)
+        {
+        }
+ 
+        public override void Add(Component component)
+        {
+            _components.Add(component);
+        }
+ 
+        public override void Remove(Component component)
+        {
+            _components.Remove(component);
+        }
+ 
+        public override void Print()
+        {
+            Console.WriteLine("Узел " + Name);
+            Console.WriteLine("Подузлы:");
+            
+            foreach (var component in _components)
+            {
+                component.Print();
+            }
+        }
+    }
+}
