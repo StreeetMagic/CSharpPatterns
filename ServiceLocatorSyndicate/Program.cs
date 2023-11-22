3870ddc99ca481f92528aa00c2089f73a8357f01
Vlad Parastaev <streeetmagic@gmail.com>
2023-11-22 15:13:15 +0400

    refactor

---
+++ ServiceLocatorSyndicate/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ServiceLocatorSyndicate
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+
+    public class ServiceLocator
+    {
+        private static ServiceLocator _instance;
+
+        public static ServiceLocator Instance => _instance ??= new ServiceLocator();
+
+        public TService RegisterSingle<TService>(TService implementation) where TService : IService
+        {
+            return Implementation<TService>.ServiceInstance = implementation;
+        }
+
+        public TService Single<TService>() where TService : IService
+        {
+            return Implementation<TService>.ServiceInstance;
+        }
+
+        private class Implementation<TService> where TService : IService
+        {
+            public static TService ServiceInstance;
+        }
+
+        public interface IService
+        {
+        }
+    }
+}
